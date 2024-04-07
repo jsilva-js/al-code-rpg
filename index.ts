cc874e494813637d40f2d0830831cc1aa4822d62
okarin <okarin504@gmail.com>
2024-04-06 22:17:16 -0300

    added char

--- index.ts
+++ index.ts
@@ -1,32 +1,24 @@
-import { cratesAfterAllMoved, cratesAfterAllReversedMoved } from './day-5/index'
-import { countFullyOverlappedSections, countOverlapsSections } from './day-4/index'
-import { getPrioritySumOfAllFailedRucksackItem, getPrioritySumOfAllGroupsBadges } from './day-3/index'
-import { countPoints, countPointsSecondStrategy } from './day-2/index'
-import { getElfCarryingMostCalories, getTopThreeElfCarryingMostCalories } from './day-1/index'
+class Char {
+  constructor(
+    // traits
+    public agility: number,
+    public strength: number,
+    public perception: number,
+    public precision: number,
 
+    // subtraits
+    public hitChance: number,
+    public evadeChance: number,
 
-describe('test all solution', () => {
-    it('should test day 1 solutions', () => {
-        expect(getElfCarryingMostCalories()).toBe(69912)
-        expect(getTopThreeElfCarryingMostCalories()).toBe(208180)
-    })
-    it('should test day 2 solutions', () => {
-        expect(countPoints()).toBe(13052)
-        expect(countPointsSecondStrategy()).toBe(13693)
-    })
+    // Attributes
+    public lifePoints: number,
+    public manaPoints: number
+  ) {}
+}
 
-    it('should test day 3 solutions', () => {
-        expect(getPrioritySumOfAllFailedRucksackItem()).toBe(7821)
-        expect(getPrioritySumOfAllGroupsBadges()).toBe(2752)
-    })
-
-    it('should test day 4 solutio1ns', () => {
-        expect(countFullyOverlappedSections()).toBe(534);
-        expect(countOverlapsSections()).toBe(841);
-    })
-    it('should test day 5 solutio1ns', () => {
-        expect(cratesAfterAllMoved()).toBe('SHMSDGZVC');
-        expect(cratesAfterAllReversedMoved()).toBe('VRZGHDFBQ');
-    })
-
-})
+describe("test all solution", () => {
+  it("should test day 1 solutions", () => {
+    expect(true).toBe(69912);
+    expect(true).toBe(208180);
+  });
+});
